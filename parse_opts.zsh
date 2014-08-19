1440dc7947c9fab13055e7a20d869393d8894559
Robin Smith <robin.smith@cloudreach.co.uk>
2014-08-18 21:54:00 -0700

    Auto-update

---
+++ parse_opts.zsh
@@ -0,0 +1,36 @@
+#!/usr/bin/env zsh
+
+# Usage: ./parse_opts.sh -l logfile -p process -o test
+
+while getopts :r:l:p:o:q:t: name
+do
+	case $name in
+		l) LOG="$OPTARG" ;;        # LOG FILE
+    p) PROC="$OPTARG" ;;       # Process name
+    o) ONE_UP="$OPTARG" ;;     # One_Up Number
+    q) PRNT="$OPTARG" ;;       # q Print queue 'FTP' for now
+    r) LIS="$OPTARG" ;;        # lis file name
+    t) FTPDEST="$OPTARG" ;;     # D = Disk, T = Tape
+    *) usage ;;                     # display usage and exit
+	esac
+done
+
+function usage {
+	echo "Usage: hello"
+}
+
+echo "LOG: $LOG"
+
+echo "PROC: $PROC"
+
+echo "ONE_UP: $ONE_UP"
+
+if [ -z "$LOG" ]
+then
+    echo "LOG is null mate!"
+fi
+
+if [ -n "$1" ]
+then
+    echo "LOG not null mate!"
+fi
