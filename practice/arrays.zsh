4ef66eab9a37999e904c22288f56238a75b083d3
Robin Smith <robin@mrrobinsmith.com>
2014-10-23 23:07:08 -0700

    Auto-update.

--- practice/arrays.zsh
+++ practice/arrays.zsh
@@ -2,9 +2,18 @@
 
 source $ZSH_HOME/lib/colors.zsh
 
-yellow "elements are separated by a space:"
+yellow "Elements are separated by a space:"
 arr=(1 2 3)
 echo $arr
 
 arr=("Hello" "Robin")
 echo $arr
+
+
+yellow "2D array:"
+arr=((1 2) (3 4))
+
+for el in $arr
+do
+  echo $el
+done
