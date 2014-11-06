ea0b7d18f7d54b1a6c88086e757ccc6540c82532
msl <robin@mrrobinsmith.com>
2014-11-06 01:17:32 -0800

    Auto-update.

---
+++ practice/pipeline_process.zsh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+source $ZSHLOG_PATH
+
+
+
+cyan "Within a pipeline, a new process is started for each portion of it up to the last (right-most) portion,
+which is run in the current shell:"
+
+lognl "cd ../../ && pwd | echo done"
+
+maganda "This is not actually an obvious point - other shells run the first (left-most) portion
+in the current shell."
+
+echo
+
+cyan "Reverse of above to show \`cd\` command running in current shell:"
+lognl "echo done | cd ../../ && pwd" 
