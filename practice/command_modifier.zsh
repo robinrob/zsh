b9a513619ab398656b9058d2816fef111adf3fc9
msl <robin@mrrobinsmith.com>
2014-11-07 13:36:32 -0800

    Auto-update.

--- practice/command_modifier.zsh
+++ practice/command_modifier.zsh
@@ -2,22 +2,40 @@
 
 source $ZSHLOG_PATH
 
-cyan "From man zshmisc"
 
-maganda "Appends ''-' to the argv string - it doesn't:"
+cyan "From man zshmisc"
+echo
+
+cyan "Appends ''-' to the argv string - it doesn't:"
+
 lognl "- echo wanker"
 
-maganda "Following command is a builtin command - fails if command is not builtin:"
+
+echo
+
+
+cyan "Following command is a builtin command - fails if command is not builtin:"
+
 lognl "builtin echo robin"
 
 lognl "builtin e robin"
 
-maganda "Following command is an external command - not shell function or builtin:"
+
+echo
+
+
+cyan "Following command is an external command - not shell function or builtin:"
+
 lognl "command ./hello.zsh"
 
 lognl "command echo wanker"
 
 lognl "command print wanker"
 
+maganda "Normally, shell functions take precedence over builtins which take precedence over external commands."
 
-maganda "noglob and nocorrect are the remaining ones - doesn't make sense to use them in a script"
+
+echo
+
+
+cyan "noglob and nocorrect are the remaining ones - doesn't make sense to use them in a script"
