9aed2c2e34975fae36321a45bf39d76171f97a04
Robin Simth <robin@mrrobinsmith.com>
2014-11-19 15:00:52 -0800

    Auto-update.

--- practice/file_exists.zsh
+++ practice/file_exists.zsh
@@ -1,16 +1,58 @@
 #!/usr/bin/env zsh
 
+source $ZSHLOG_PATH
 
+
+logb '
 if [ -f file_exists.sh ]
 then
 	echo "yes!"
 else
 	echo "no!"
 fi
+'
 
+
+log
+
+
+cyan 'The -f check is case-INsensitive.'
+
+logb '
 if ! [ -f file_exists.sh ]
 then
 	echo "yes!"
 else
 	echo "no!"
+fi
+'
+
+log
+
+
+function file_exists {
+  FILENAME=$1
+  EXISTS=$(find . -name $FILENAME)
+
+  if [[ $EXISTS != '' ]]
+  then
+    echo 'yes'
+  else
+    echo 'no'
+  fi
+}
+
+
+file_exists file_exists.zsh
+file_exists file_existS.zsh
+
+
+log
+
+
+if [[ "$(file_exists file_exists.zsh)" == "yes" ]]
+then
+  print "does exist"
+else
+  print "does not exist"
 fi
