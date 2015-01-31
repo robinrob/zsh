5842b12f5a2a986e3f8505c9ecc6f7235a34bd05
msl <msl@mercury.local>
2015-01-31 15:26:38 +0000

    Auto-update.

--- practice/sed/sed_all.zsh
+++ practice/sed/sed_all.zsh
@@ -1,3 +1,9 @@
 #!/usr/bin/env zsh
 
-sed_all 's/robin/smith/g' '*'
+[[ $fpath = *dotfiles-base* ]] || fpath=($FUNCS_HOME $fpath)
+# -U prevents alias expansion during function definition.
+# +X loads the file immediately - rather than only when it is needed. This
+# allows the function's contents to be seen by running e.g. `which bastard`.
+autoload -U +X $fpath[1]/*(:t) 2> /dev/null
+
+sed_all 's/smith/smith/g' '*'
