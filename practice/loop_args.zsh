e9e52751e33c0fc25d081b1fbf07e52ea7c868d4
Robin Smith <robin@rsmith.io>
2016-07-10 19:55:01 +0100

    Quick-save.

--- practice/loop_args.zsh
+++ practice/loop_args.zsh
@@ -1,16 +1,39 @@
 #!/usr/bin/env zsh
 
+
 args="one two three"
-
-for arg in $args
+for arg in ${=args}
 do
-	echo "arg: $arg"
+	print "arg: $arg"
 done
 
-args=${=args}
-echo "args: $args"
+print
 
+
+set one two three
+
+args=($*)
 for arg in $args
 do
-	echo "arg: $arg"
+	print "arg: $arg"
 done
+
+
+print
+
+local +r args=($*)
+for arg in $args
+do
+	print "arg: $arg"
+done
+
+
+print
+
+
+local +r -a args=($*)
+for arg in $args
+do
+	print "arg: $arg"
+done
+
